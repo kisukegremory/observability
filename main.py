f7d7497266fcc2d807b3aa11f64bcf4603349775
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2026-08-14 18:20:26 -0300

    feat: custom logic for tracing comparison

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from opentelemetry import trace
+import time
+import random
+from uuid import uuid4
 
 app = FastAPI()
+tracer = trace.get_tracer(__name__)
 
 
 @app.get("/health")
 def main():
-    return {"Nina":"Nyaaaa"}
+    with tracer.start_as_current_span("processamento_leve"):
+        return {"Nina":"Nyaaaa"}
+
+@tracer.start_as_current_span("calculo_coxinhas")
+def calcula_coxinhas():
+    span = trace.get_current_span()
+    span.set_attribute("user.id",str(uuid4()))
+
+
+@app.get("/work")
+def work():
+    with tracer.start_as_current_span("processamento_brabo") as span:
+        time_choosed = random.randint(1,5)
+        if time_choosed < 3:
+            time.sleep(time_choosed)
+            calcula_coxinhas()
+            span.set_attribute("nina.estado","trabalhando")
+            return {"Nina":"Cansadinina!"}
+        span.set_attribute("nina.estado","faz_nada")
+        return {"Nina":"Me da petisco!"}
