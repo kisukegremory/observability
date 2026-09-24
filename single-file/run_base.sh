ecafb02506764d6b5c373b11042d8c8f0ec931c6
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2026-09-24 14:09:44 -0300

    fix(single-file): adapted base example

--- single-file/run_base.sh
+++ single-file/run_base.sh
@@ -1,8 +1,9 @@
 export OTEL_SERVICE_NAME=obs-single-file
-export OTEL_LOGS_EXPORTER=none
+export OTEL_LOGS_EXPORTER=console
+export OTEL_PYTHON_LOG_LEVEL=debug
 export OTEL_TRACES_EXPORTER=none
 export OTEL_METRICS_EXPORTER=none
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
 
 
-uv run opentelemetry-instrument python main_traced.py
+uv run opentelemetry-instrument python main.py
