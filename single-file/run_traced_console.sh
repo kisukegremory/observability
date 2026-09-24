dcb5e55235368c29d5cb66f8cd091d1a7203c68a
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2026-09-24 15:01:02 -0300

    fix(single-file): adapted base example

---
+++ single-file/run_traced_console.sh
@@ -0,0 +1,11 @@
+export OTEL_SERVICE_NAME=obs-single-file
+export OTEL_LOGS_EXPORTER=console
+#export OTEL_PYTHON_LOG_FORMAT="%(asctime)s %(levelname)s [%(name)s] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s] %(message)s"
+export OTEL_PYTHON_LOG_CORRELATION=true
+export OTEL_PYTHON_LOG_LEVEL=debug
+export OTEL_TRACES_EXPORTER=none
+export OTEL_METRICS_EXPORTER=none
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
+
+
+uv run opentelemetry-instrument python main_traced.py
